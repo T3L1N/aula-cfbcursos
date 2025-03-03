fd798cdffa90ac453c2d96f8fce965de64c508e5
Jo├úo Teles <joaohts2210@gmail.com>
2025-03-03 15:24:56 -0300

    aprendendo mais alguns metodos de array

--- aula/aula47/index.js
+++ aula/aula47/index.js
@@ -8,5 +8,12 @@ const elementos_array = [10, 5, 8, 2, 9, 15, 20]
 p_array.innerHTML = `[${elementos_array}]`
 
 btnPesquisar.addEventListener('click', (evt) => {
-    elementos_array.find()
+    const ret = elementos_array.find((e, i) => {
+        if (e == pesquisar.value) {
+            resultado.innerHTML = `Valor encontrado ${e}, na posição ${i}`
+            return e
+        } else {
+            resultado.innerHTML = `Esse valor não existe`
+        }
+    })
 })
