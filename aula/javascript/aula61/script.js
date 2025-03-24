73baec83b48b0181fb47f21c2d32e77a9db5e865
Jo├úo Teles <joaohts2210@gmail.com>
2025-03-24 16:07:04 -0300

    temrinar aula 63

--- aula/javascript/aula61/script.js
+++ aula/javascript/aula61/script.js
@@ -1,27 +1,11 @@
-const Pessoa = {
-    nome,
-    idade,
-    getNome: function() {
-        return this.nome
-    },
-    getIdade: function() {
-        return this.idade
-    },
-    setNome: function(nome) {
-        this.nome = nome
-    },
-    setNome: function(idade) {
-        this.idade = idade
-    }
-}
+const Pnome = document.querySelector('#fnome')
+const Pidade = document.querySelector('#fidade')
+const btnADD = document.querySelector('#btnADD')
+const res = document.querySelector('.res')
 
-const p2 = Pessoa
-const p3 = Pessoa
-
-p3.nome = 'Cladinelson'
-p2['nome'] = 'Juvenal'
-Pessoa.setNome('Jucelaine')
-
-console.log(Pessoa.nome)
-console.log(p2.getNome())
-console.log(p3.nome)
+btnADD.addEventListener('click', (evt) => {
+    nm = Pnome.value
+    idd = Pidade.value
+    
+    res.innerHTML += `${nm}: ${idd} <br>`
+})
---
+++ aula/javascript/aula63/script.js
@@ -0,0 +1,83 @@
+const ftipoMilitar = document.querySelector('#ftipoMilitar')
+const normal = document.querySelector('#ftipoNormal')
+
+ftipoMilitar.addEventListener('click', (evt) => {
+    console.log('teste')
+})
+
+ftipoNormal.addEventListener('click', (evt) => {
+    console.log('teste')
+})
+
+class Carro { //Classe PAI / BASE
+    constructor (nome, portas) {
+        this.nome = nome
+        this.portas = portas
+        this.ligado = false
+        this.vel = 0
+        this.cor = ''
+    }
+    ligar = function() {
+        this.ligado = true
+    }
+    desligar = function() {
+        this.ligado =false
+    }
+    setCor = function(cor) {
+        this.cor = cor
+    }
+}
+
+class Militar extends Carro { //class FILHO
+    constructor(nome, portas, blindagem, municao) {
+        super(nome, portas)
+        this.blindagem = blindagem
+        this.municao = municao
+        this.setCor('Verde')
+    }
+    atirar = function() {
+        if(this.municao > 0) {
+            this.municao--
+        }
+    }
+}
+
+class Utilitario extends Carro {
+    constructor(nome, portas, lugares) {
+        super(nome, portas)
+        this.lugares = lugares
+    }
+}
+
+const c1 = new Carro('Civic', 4)
+c1.ligar()
+c1.setCor('Preto')
+
+const c2 = new Militar('Lutador', 6, 100, 50)
+
+const c3 = new Utilitario('Prisma', 4, 5)
+c3.setCor('Preto')
+
+console.log(`Nome: ${c1.nome}`)
+console.log(`Portas: ${c1.portas}`)
+console.log(`Ligado: ${(c1.ligado)?'Sim' : 'Não'}`)
+console.log(`Velocidade: ${c1.vel}`)
+console.log(`Cor: ${c1.cor}`)
+console.log(`----------------`)
+
+console.log(`Nome: ${c2.nome}`)
+console.log(`Portas: ${c2.portas}`)
+console.log(`Ligado: ${(c2.ligado)?'Sim' : 'Não'}`)
+console.log(`Velocidade: ${c2.vel}`)
+console.log(`Blindagem: ${c2.blindagem}`)
+console.log(`Munição: ${c2.municao}`)
+console.log(`Cor: ${c2.cor}`)
+console.log(`----------------`)
+
+console.log(`Nome: ${c3.nome}`)
+console.log(`Portas: ${c3.portas}`)
+console.log(`Lugares: ${c3.lugares}`)
+console.log(`Ligado: ${(c3.ligado)?'Sim' : 'Não'}`)
+console.log(`Velocidade: ${c3.vel}`)
+console.log(`Cor: ${c3.cor}`)
+console.log(`----------------`)
